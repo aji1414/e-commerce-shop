03edbb57367f3ccd850292e922f1583f93880605
Ajibola Lawal <ajisofteng@gmail.com>
2021-01-09 13:43:07 +0000

    add empty cart confirmation dialog modal to cartdropdown component

--- client/src/Components/CartDropdown/CartDropdown.component.jsx
+++ client/src/Components/CartDropdown/CartDropdown.component.jsx
@@ -1,8 +1,9 @@
 // external libraries and stylesheet
-import React from "react";
+import React, {Component} from "react";
 import {CartTotal, CartDropdownContainer, CartItemsContainer, EmptyMessageContainer, ClearButtonContainer} from "./CartDropdown.styles";
-// import "./CartDropdown.styles.scss";
 import {withRouter} from "react-router-dom";
+import {confirmAlert} from "react-confirm-alert";
+import 'react-confirm-alert/src/react-confirm-alert.css';
 
 // redux
 import {connect} from "react-redux";
@@ -14,37 +15,63 @@ import {toggleCartHidden, clearCart} from "../../Redux/Cart/Cart.actions";
 import CustomButton from "../CustomButton/CustomButton.component";
 import CartItem from "../CartItem/CartItem.component";
 
-const CartDropdown = ({cartTotal,cartItems, history, dispatch}) => (
-    <CartDropdownContainer>
-        <CartItemsContainer>
-            {
-            cartItems.length ?
-            cartItems.map(cartItem => (
-                <CartItem key={cartItem.id} item={cartItem} />
-            ))
-            :
-            <EmptyMessageContainer>Your cart is empty</EmptyMessageContainer>
-            }
-        </CartItemsContainer>
-        {
-            cartItems.length &&
-            <CartTotal>
-                Current Total: ${cartTotal}
-            </CartTotal>
-        }
-        <CustomButton onClick={() => {
-            history.push("/checkout");
-            dispatch(toggleCartHidden())}}>
-            GO TO CHECKOUT
-        </CustomButton>
-        <ClearButtonContainer onClick={() =>{
-            dispatch(clearCart());
-            dispatch(toggleCartHidden());
-        }}>
-            Empty Cart
-        </ClearButtonContainer>
-    </CartDropdownContainer>
-)    
+// ({cartTotal,cartItems, history, dispatch})
+
+class CartDropdown extends Component {
+    emptyCartSubmit = () => {
+        const {dispatch} = this.props;
+
+        confirmAlert({
+            title: 'Are you sure you want to empty your cart?',
+            buttons: [
+              {
+                label: 'Yes, proceed!',
+                onClick: (output) =>{
+                    dispatch(clearCart());
+                }
+              },
+              {
+                label: 'No, go back',
+                onClick: (output) => {return}
+              }
+            ]
+          });
+    };
+    
+    render(){
+        const {cartTotal, cartItems, history, dispatch} = this.props;
+        return(
+            <CartDropdownContainer>
+                <CartItemsContainer>
+                    {
+                    cartItems.length ?
+                    cartItems.map(cartItem => (
+                        <CartItem key={cartItem.id} item={cartItem} />
+                    ))
+                    :
+                    <EmptyMessageContainer>Your cart is empty</EmptyMessageContainer>
+                    }
+                </CartItemsContainer>
+                {
+                    cartItems.length &&
+                    <CartTotal>
+                        Current Total: ${cartTotal}
+                    </CartTotal>
+                }
+                <CustomButton onClick={() => {
+                    history.push("/checkout");
+                    dispatch(toggleCartHidden())}}>
+                    GO TO CHECKOUT
+                </CustomButton>
+                <ClearButtonContainer onClick={async () =>{
+                    this.emptyCartSubmit();
+                }}>
+                    Empty Cart
+                </ClearButtonContainer>
+            </CartDropdownContainer>
+        )
+    }
+}    
 
 
 const mapStateToProps = createStructuredSelector({
